27603d2ae8ce95876a38f42f96d26219f58d48b4
Neting <532501838@qq.com>
2022-10-30 00:32:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-ten.vercel.app',
+    serverName: 'siteproxy-black.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
